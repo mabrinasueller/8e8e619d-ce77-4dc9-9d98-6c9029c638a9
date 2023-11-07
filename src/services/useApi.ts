5694f1d7d4f8983c32adcad5fba32885673eae30
Sabrina Müller <sabrina.mueller@diva-e.com>
2023-11-07 11:49:07 +0100

    clean up code and remove unnecessary component file

--- src/services/useApi.ts
+++ src/services/useApi.ts
@@ -1,42 +1,13 @@
-// import axios from "axios";
-
-// export function useAPI() {
-//   const apiClient = axios.create({
-//     baseURL: "https://teclead-ventures.github.io",
-//     withCredentials: false,
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//   });
-
-//   async function fetchData() {
-//     return await apiClient
-//       .get("/data/london-events.json")
-//       .then((response) => {
-//         console.log("Response", response.data);
-//         return response.data;
-//       })
-//       .catch((error) => {
-//         console.log("Error", error);
-//         throw error;
-//       });
-//   }
-// }
-
-// api.ts
-
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://teclead-ventures.github.io", // Replace with your API base URL
+  baseURL: "https://teclead-ventures.github.io",
 });
 
 export function fetchData() {
   return axiosInstance
-    .get("/data/london-events.json") // Replace with your specific API endpoint
+    .get("/data/london-events.json")
     .then((response) => {
-      console.log("Response", response.data);
       return response.data;
     })
     .catch((error) => {
