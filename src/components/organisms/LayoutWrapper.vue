2e6bdea5f6a22868101b402855216517d042ed28
Sabrina Müller <sabrina.mueller@diva-e.com>
2023-11-07 11:51:56 +0100

    clean up code, add styles for header and general styles

--- src/components/organisms/LayoutWrapper.vue
+++ src/components/organisms/LayoutWrapper.vue
@@ -4,7 +4,6 @@
       <RouterLink to="/"> </RouterLink>
     </HeaderComponent>
     <main>
-      <slot />
       <EventCollection></EventCollection>
     </main>
   </div>
@@ -13,14 +12,8 @@
 <script setup lang="ts">
 import HeaderComponent from "@/components/molecules/HeaderComponent.vue";
 import EventCollection from "@/components/molecules/EventCollection.vue";
-import DateComponent from "@/components/molecules/DateComponent.vue";
 </script>
 
 <style scoped lang="scss">
-.layout-wrapper {
-  position: relative;
-  min-height: 100vh;
-  margin-inline: auto;
-  overflow: hidden;
-}
+@import "@/scss/styles.scss";
 </style>
