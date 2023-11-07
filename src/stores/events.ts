1dddb190c1485f87c9be2422290946eb7f4b40a2
Sabrina Müller <sabrina.mueller@diva-e.com>
2023-11-07 11:52:46 +0100

    adjust actions to properly sort fetched events from API and group them into corresponding dates

--- src/stores/events.ts
+++ src/stores/events.ts
@@ -1,15 +1,69 @@
 import { defineStore } from "pinia";
-import { reactive } from "vue";
 import { fetchData } from "@/services/useApi";
 import { Event } from "../types/types";
-import { ref, computed } from "vue";
-const api = fetchData();
 
-export const useEventStore = defineStore("myStore", {
+type CartEvents = string[];
+type CartTitles = string[];
+
+interface GroupedEvents {
+  [date: string]: Event[];
+}
+
+export const useEventsStore = defineStore("events", {
   state: () => ({
-    // Define your store state here
+    groupedEvents: {} as Record<string, Event[]>,
+    cartItemCount: 0,
+    cartEvents: [] as CartEvents,
+    cartTitles: [] as CartTitles,
   }),
   actions: {
-    // Define your store actions here
+    async fetchEvents(inputValue = "") {
+      try {
+        const data = await fetchData();
+
+        const groupedEventsData: GroupedEvents = data.reduce(
+          (acc: GroupedEvents, event: Event) => {
+            const date = new Date(event.date).toLocaleDateString();
+
+            if (!acc[date]) {
+              acc[date] = [];
+            }
+
+            if (
+              !inputValue ||
+              (event.title.toLowerCase().includes(inputValue.toLowerCase()) &&
+                !this.cartEvents.some((cartEvent) => cartEvent === event._id))
+            ) {
+              acc[date].push(event);
+            }
+
+            return acc;
+          },
+          {} as GroupedEvents
+        );
+
+        const sortedGroupedEvents: GroupedEvents = Object.values(
+          groupedEventsData
+        )
+          .flat()
+          .sort((a, b) => {
+            const dateA = new Date(a.date).getTime();
+            const dateB = new Date(b.date).getTime();
+            return dateA - dateB;
+          })
+          .reduce((acc: GroupedEvents, event: Event) => {
+            const date = new Date(event.date).toLocaleDateString();
+            if (!acc[date]) {
+              acc[date] = [];
+            }
+            acc[date].push(event);
+            return acc;
+          }, {} as GroupedEvents);
+
+        this.groupedEvents = sortedGroupedEvents;
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    },
   },
 });
